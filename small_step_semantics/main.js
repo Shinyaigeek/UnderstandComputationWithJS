610d2c276be90ccecf3a854284d2db210a27cb56
svpcadmin <PhilisPaxil@gmail.com>
2019-11-28 23:35:26 +0900

    [add]mini code

---
+++ small_step_semantics/main.js
@@ -0,0 +1,62 @@
+class Number {
+  constructor(value) {
+    this.value = value;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  toString() {
+    return this.getValue().toString();
+  }
+
+  inspect() {
+    return `<< ${this} >>`;
+  }
+}
+
+class Add {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  returnValue() {
+    return this.left + this.right;
+  }
+
+  toString() {
+    return `${this.left} + ${this.right}`;
+  }
+
+  inspect() {
+    return `<< ${this} >>`;
+  }
+}
+
+class Multipy {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  returnValue() {
+    return this.left * this.right;
+  }
+
+  toString() {
+    return `${this.left} * ${this.right}`;
+  }
+
+  inspect() {
+    return `<< ${this} >>`;
+  }
+}
+
+console.log(
+    new Add(
+        new Multipy(new Number(1),new Number(2)),
+        new Multipy(new Number(3),new Number(4)),
+    )
+)
