39d7b85de64602fc6ee2af89a391f19dcba20624
svpcadmin <PhilisPaxil@gmail.com>
2019-11-29 19:01:38 +0900

    [add]variables

--- small_step_semantics/main.js
+++ small_step_semantics/main.js
@@ -24,17 +24,17 @@ class Add {
     this.reducible = true;
   }
 
-  reduce() {
+  reduce(environment) {
     if (this.left.reducible) {
-      return new Add(this.left.reduce(), this.right);
+      return new Add(this.left.reduce(environment), this.right);
     } else if (this.right.reducible) {
-      return new Add(this.left, this.right.reduce());
+      return new Add(this.left, this.right.reduce(environment));
     } else {
       return new Number(this.left.getValue() + this.right.getValue());
     }
   }
 
-  returnValue() {
+  getValue() {
     return this.left + this.right;
   }
 
@@ -43,7 +43,7 @@ class Add {
   }
 
   inspect() {
-    return `<< ${this} >>`;
+    return `<< ${this.left.getValue()} + ${this.right.getValue()} >>`;
   }
 }
 
@@ -54,17 +54,17 @@ class Multiply {
     this.reducible = true;
   }
 
-  reduce() {
+  reduce(environment) {
     if (this.left.reducible) {
-      return new Multiply(this.left.reduce(), this.right);
+      return new Multiply(this.left.reduce(environment), this.right);
     } else if (this.right.reducible) {
-      return new Multiply(this.left, this.right.reduce());
+      return new Multiply(this.left, this.right.reduce(environment));
     } else {
       return new Number(this.left.getValue() * this.right.getValue());
     }
   }
 
-  returnValue() {
+  getValue() {
     return this.left * this.right;
   }
 
@@ -73,56 +73,56 @@ class Multiply {
   }
 
   inspect() {
-    return `<< ${this} >>`;
+    return `<< ${this.left.getValue()} * ${this.right.getValue()} >>`;
   }
 }
 
 class Boolean {
-    constructor(value){
-        this.value = value;
-        this.reducible = false;
-    }
+  constructor(value) {
+    this.value = value;
+    this.reducible = false;
+  }
 
-    getValue(){
-        return this.value;
-    }
+  getValue() {
+    return this.value;
+  }
 
-    inspect(){
-        return `<< ${this.value} >>`;
-    }
+  inspect() {
+    return `<< ${this.value} >>`;
+  }
 }
 
 class LessThan {
-    constructor(left,right){
-        this.left = left;
-        this.right = right;
-        this.reducible = true;
-    }
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+    this.reducible = true;
+  }
 
-    inspect(){
-        return `<< ${this.left} < ${this.right} >>`
-    }
+  inspect() {
+    return `<< ${this.left} < ${this.right} >>`;
+  }
 
-    reduce(){
-        if(this.left.reducible){
-            return new LessThan(this.left.reduce(),this.right);
-        }else if(this.right.reducible){
-            return new LessThan(this.left,this.right.reduce());
-        }else{
-            return new Boolean(this.left.getValue() < this.right.getValue())
-        }
+  reduce(environment) {
+    if (this.left.reducible) {
+      return new LessThan(this.left.reduce(environment), this.right);
+    } else if (this.right.reducible) {
+      return new LessThan(this.left, this.right.reduce(environment));
+    } else {
+      return new Boolean(this.left.getValue() < this.right.getValue());
     }
-
+  }
 }
 
 class Machine {
-  constructor(expression) {
+  constructor(expression,environment) {
     this.expression = expression;
+    this.environment = environment
   }
 
   step() {
     console.log(this.expression.inspect());
-    this.expression = this.expression.reduce();
+    this.expression = this.expression.reduce(this.environment);
   }
 
   run() {
@@ -133,14 +133,31 @@ class Machine {
   }
 }
 
+class Variable {
+    constructor(key){
+        this.key = key;
+        this.reducible = true;
+    }
+
+    inspect(){
+        return `<< ${this.getKey()} >>`
+    }
+
+    getValue(){
+        return this.key;
+    }
+
+    reduce(environment){
+        return environment[this.getValue()];
+    }
+}
+
 console.log(
   new Add(
     new Multiply(new Number(1), new Number(2)),
     new Multiply(new Number(3), new Number(4))
   )
 );
-
-console.log(new Add(new Number(1), new Number(2)).reduce());
 
 console.log(
   new Machine(
@@ -149,25 +166,25 @@ console.log(
 );
 
 console.log(
+  new Machine(
+    new LessThan(
+      new Multiply(new Add(new Number(1), new Number(2)), new Number(3)),
+      new Add(
+        new Multiply(new Number(4), new Multiply(new Number(5), new Number(6))),
+        new Number(7)
+      )
+    )
+  ).run()
+);
+
+console.log(
     new Machine(
-        new LessThan(
-            new Multiply(
-                new Add(
-                    new Number(1),
-                    new Number(2)
-                ),
-                new Number(3)
-            ),
-            new Add(
-                new Multiply(
-                    new Number(4),
-                    new Multiply(
-                        new Number(5),
-                        new Number(6)
-                    )
-                ),
-                new Number(7)
-            )
-        )
+        new Add(
+            new Variable("x"),new Variable("y")
+        ),
+        {
+            x: new Number(1),
+            y: new Number(4)
+        }
     ).run()
 )
